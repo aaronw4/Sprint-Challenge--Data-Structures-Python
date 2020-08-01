ef590d051f20a7443df162d176fc732502b7de4f
Aaron Williams <aaronrw4@yahoo.com>
2020-07-31 20:45:31 -0700

    Finished names with runtime 0.06 seconds baby!!!

--- names/names.py
+++ names/names.py
@@ -13,10 +13,60 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+class BSTNode:
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
+
+    def insert(self, name):
+        curr_node = self
+
+        while curr_node is not None:
+            if name < curr_node.name:
+                if curr_node.left is None:
+                    curr_node.left = BSTNode(name)
+                    curr_node = None
+                else:
+                    curr_node = curr_node.left
+            elif name >= curr_node.name:
+                if curr_node.right is None:
+                    curr_node.right = BSTNode(name)
+                    curr_node = None
+                else: 
+                    curr_node = curr_node.right
+
+    def contains(self, target):
+        curr_node = self
+
+        while curr_node is not None:
+            if curr_node.name == target:
+                return True
+            elif target < curr_node.name:
+                if curr_node.left is None:
+                    return False
+                else:
+                    curr_node = curr_node.left
+            elif target >= curr_node.name:
+                if curr_node.right is None:
+                    return False
+                else: 
+                    curr_node = curr_node.right
+
+first_list = BSTNode(names_1[0])
+
+for x in range(1, len(names_1)):
+    first_list.insert(names_1[x])
+
+for name_2 in  names_2:
+    if first_list.contains(name_2):
+        duplicates.append(name_2)
+
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
